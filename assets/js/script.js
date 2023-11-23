ed4dbc7f144682aaa231a9cd9dc2298961697eff
Ugochiee <uwaomaugochinyerem@gmail.com>
2023-11-23 17:28:14 +0100

    update features dropdown; add mobile nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
-const featuresLink = document.querySelector(".features");
+const featuresLink = document.querySelector("#features");
 const featuresContainer = document.querySelector(".features-container");
+const nav = document.querySelector(".nav");
 
-featuresLink.addEventListener("hover", () => {
-  featuresContainer.style.visibility = "visible";
+featuresLink.addEventListener("click", () => {
+  toggleFeaturesContainer();
 });
+
+const toggleFeaturesContainer = () => {
+  if (featuresContainer.style.visibility == "visible") {
+    featuresContainer.style.visibility = "hidden";
+    nav.style.height = "100px";
+  } else {
+    featuresContainer.style.visibility = "visible";
+    nav.style.height = "auto";
+  }
+};
